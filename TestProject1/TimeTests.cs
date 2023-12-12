b412e7eed762a26eaf60f1a77ca433e2540a2508
Thomas Gugler <thomas.gugler@nagarro.com>
2023-12-12 12:48:45 +0100

    PaydayCalculator added + tests

---
+++ TestProject1/TimeTests.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibrary1;
+using FluentAssertions;
+using NSubstitute;
+using Xunit.Abstractions;
+using static ClassLibrary1.NET8;
+
+namespace TestProject1;
+
+public class TimeTests(ITestOutputHelper testOutputHelper)
+{
+    [Theory]
+    [InlineData("2023-01-30")]
+    [InlineData("2023-02-01")]
+    [InlineData("2023-02-27")]
+    [InlineData("2023-03-01")]
+    [InlineData("2023-04-30")]
+    [InlineData("2023-09-30")]
+    [InlineData("2023-12-31")]
+    public void Is_Is_Not_Payday(string date)
+    {
+        // Arrange
+        var startDateTime = new DateTimeOffset(DateOnly.Parse(date), TimeOnly.MinValue, TimeSpan.Zero);
+        // var timeProvider = new FakeTimeProvider(startDateTime);
+        // var mock = new Mock<TimeProvider>();
+        var mocktimeProvider = Substitute.For<TimeProvider>();
+        mocktimeProvider.GetUtcNow().Returns(startDateTime);
+        var calculator = new PaydayCalculator(mocktimeProvider);
+        // Act
+        var actual = calculator.IsPayday();
+        // Assert
+        actual.Should().BeFalse();
+    }
+}
