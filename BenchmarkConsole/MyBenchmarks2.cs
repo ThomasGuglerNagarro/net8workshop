97ca279251f64d824bcc8f4da3cc82372023d913
Thomas Gugler <thomas.gugler@nagarro.com>
2023-12-13 15:58:27 +0100

    added stats

--- BenchmarkConsole/MyBenchmarks2.cs
+++ BenchmarkConsole/MyBenchmarks2.cs
@@ -2,7 +2,12 @@
 using ClassLibrary1;
 
 namespace BenchmarkConsole;
-
+/*
+ Method                      | Mean      | Error    | StdDev   | Gen0   | Allocated |
+|---------------------------- |----------:|---------:|---------:|-------:|----------:|
+| CalculateAverageAgeWithSpan |  99.07 ns | 1.160 ns | 1.028 ns | 0.0408 |     256 B |
+| CalculateAverageAge         | 180.07 ns | 0.903 ns | 0.754 ns | 0.0994 |     624 B |
+ */
 [MemoryDiagnoser]
 public class MyBenchmarks2
 {
